81cb80f2c70879422cb8f9fd1fa448c8a9148b7b
notrichardpeng <notrichardpeng@gmail.com>
2025-04-06 17:17:22 -0700

    Infrastructure Setup

---
+++ spring-boot-backend/uscstudyspotfinder/src/main/java/com/uscstudyspotfinder/UscstudyspotfinderApplication.java
@@ -0,0 +1,13 @@
+package com.uscstudyspotfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UscstudyspotfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UscstudyspotfinderApplication.class, args);
+	}
+
+}
