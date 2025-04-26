7d054b0c48aaad43d30a1733d9fd1f985202106f
Xiaoran Zou <charlottezou0410@163.com>
2025-04-26 16:24:28 -0700

    fixed voteController.java

--- spring-boot-backend/uscstudyspotfinder/src/main/java/com/uscstudyspotfinder/controller/VoteController.java
+++ spring-boot-backend/uscstudyspotfinder/src/main/java/com/uscstudyspotfinder/controller/VoteController.java
@@ -2,6 +2,17 @@ package com.uscstudyspotfinder.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.uscstudyspotfinder.repository.UserRepository;
+import com.uscstudyspotfinder.repository.StudySpotRepository;
+import com.uscstudyspotfinder.model.StudySpot;
+import com.uscstudyspotfinder.repository.VoteRepository;
+import com.uscstudyspotfinder.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import java.util.Optional;
 
 @RestController
 public class VoteController {
@@ -21,16 +32,12 @@ public class VoteController {
         Optional<User> userOpt = userRepository.findById(userId);
         Optional<StudySpot> spotOpt = spotRepository.findById(spotId);
 
-        if (userOpt.isPresent() && studyOpt.isPresent())
+        if (userOpt.isPresent() && spotOpt.isPresent())
         {
-            voteRepository.addVote(userId, spotId);
-            return "Successfully added vote";
+            return ResponseEntity.ok("Successfully added vote");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User or spot not found.");
         }
-        else
-        {
-            return "User or spot not found.";
-        }
-
     }
 
 }
