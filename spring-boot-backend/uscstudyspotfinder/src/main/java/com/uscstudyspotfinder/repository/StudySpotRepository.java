4322950a5cc5dfebe62574c8c924b735b2536e00
notrichardpeng <notrichardpeng@gmail.com>
2025-04-26 16:29:00 -0700

    more fixes

--- spring-boot-backend/uscstudyspotfinder/src/main/java/com/uscstudyspotfinder/repository/StudySpotRepository.java
+++ spring-boot-backend/uscstudyspotfinder/src/main/java/com/uscstudyspotfinder/repository/StudySpotRepository.java
@@ -3,6 +3,8 @@ package com.uscstudyspotfinder.repository;
 import com.uscstudyspotfinder.model.StudySpot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudySpotRepository extends JpaRepository<StudySpot, Long> {
 
     Optional<StudySpot> findById(Long id);
--- spring-boot-backend/uscstudyspotfinder/src/main/java/com/uscstudyspotfinder/repository/VoteRepository.java
+++ spring-boot-backend/uscstudyspotfinder/src/main/java/com/uscstudyspotfinder/repository/VoteRepository.java
@@ -1,9 +1,10 @@
 package com.uscstudyspotfinder.repository;
 
+import com.uscstudyspotfinder.model.StudySpot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VoteRepository extends JpaRepository<Long> {
+public interface VoteRepository extends JpaRepository<StudySpot, Long> {
 
     void addVote(Long userId, Long spotId);
 
