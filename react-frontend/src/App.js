be9d72ecc442cbb7b6b50b0da767fd9c8b0da4c0
Richard (Jiankun) Peng <58278719+notrichardpeng@users.noreply.github.com>
2025-05-02 18:11:57 -0700

    Revert "myspots skeleton"

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,52 +5,23 @@ import SignupPage from './components/SignupPage';
 import MainPage from './components/MainPage';
 import { Navbar } from './components/Navbar';
 import { AuthProvider } from './contexts/AuthContext';
-import { MySpots } from './components/MySpots';
-import { Friend } from './components/Friend';
 
 function App() {
   const [friendOpen, setFriendOpen] = useState(false);
-  const [spotsOpen, setSpotsOpen] = useState(false);
-
   const toggleFriend = (e) => {
     if (e) e.stopPropagation();
     setFriendOpen(open => !open);
-    setSpotsOpen(false); // Close spots when opening friends
-  };
-
-  const toggleSpots = (e) => {
-    if (e) e.stopPropagation();
-    setSpotsOpen(open => !open);
-    setFriendOpen(false); // Close friends when opening spots
   };
 
   return (
     <AuthProvider>
       <Router>
-        <Navbar 
-          friendOpen={friendOpen} 
-          toggleFriend={toggleFriend}
-          spotsOpen={spotsOpen}
-          toggleSpots={toggleSpots}
-        />
+        <Navbar friendOpen={friendOpen} toggleFriend={toggleFriend} />
         <Routes>
-          <Route 
-            path="/" 
-            element={
-              <>
-                <Friend isOpen={friendOpen} toggleDashboard={toggleFriend} />
-                <MySpots isOpen={spotsOpen} toggleDashboard={toggleSpots} />
-                <MainPage 
-                  friendOpen={friendOpen} 
-                  toggleFriend={toggleFriend}
-                  spotsOpen={spotsOpen}
-                  toggleSpots={toggleSpots}
-                />
-              </>
-            } 
-          />
+          <Route path="/" element={<MainPage friendOpen={friendOpen} toggleFriend={toggleFriend} />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/spots" element={<MainPage friendOpen={friendOpen} toggleFriend={toggleFriend} />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
